92319c7b08e019e118de843dd2e683434ab5d470
eiler2005 <denis.ermilov@gmail.com>
2018-11-22 23:02:49 +0300

    rsi

---
+++ BacktestProj/src/ru/backtesting/signal/RSITechnicalSignal.java
@@ -0,0 +1,13 @@
+package ru.backtesting.signal;
+
+import java.time.LocalDateTime;
+
+public class RSITechnicalSignal implements SignalTestingAction {
+
+	@Override
+	public int testSignal(LocalDateTime date, String ticker) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
